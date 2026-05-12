6cf9f6d54325f05929105daa416bc5e3d3c60dec
codechini <arangathvenuchinmay@gmail.com>
2026-05-12 22:49:25 +0530

    Cors Patch fix

--- back-end/main.py
+++ back-end/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 from database import db_manager
 from routes import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+   )
 @app.on_event("startup")
 def startup():
     db_manager.connect(["localhost"], "order_keyspace")
