097b517f7581858c9534ae1348deff4c2e28964d
MisbahArthur <misbahulhaque127@gmail.com>
2026-05-12 22:26:34 +0530

    Updated BaseModel class

--- back-end/models.py
+++ back-end/models.py
@@ -1,18 +1,18 @@
 from typing import Optional
 from datetime import datetime
 from decimal import Decimal
-from pydantic import BaseModel
-import uuid
+from pydantic import BaseModel, Field
+
 
 
 class OrderTracking(BaseModel):
-    order_id: uuid.UUID
-    repair_id: uuid.UUID
+    # order_id: uuid.UUID
+    # repair_id: uuid.UUID
     customer_name: str
     repair_device: str
-    order_creation: uuid.UUID
+    # order_creation: uuid.UUID
     repair_cost: Decimal
     repair_start: Optional[datetime] = None
     repair_finish: Optional[datetime] = None
-    repair_status: str
+    repair_status: str = Field(..., pattern=r"^(Picked-up|Work in progress|picking up parts|Finished)$")
     repair_eta: str
